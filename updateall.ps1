ef37c465ea736f30f8b75c14bc2fb7debdcca6c7
Lucas Teo <86002658+teoshibin2@users.noreply.github.com>
2022-12-11 23:22:24 +0000

    pin yacl update for 1.19.2

--- updateall.ps1
+++ updateall.ps1
@@ -1,6 +1,6 @@
 packwiz update --all
 Write-Output "Undo Ignored Mods Update"
 git checkout HEAD -- .\mods\memoryleakfix.pw.toml
-# git checkout HEAD -- .\mods\createaddition.pw.toml
+git checkout HEAD -- .\mods\yacl.pw.toml
 packwiz refresh
 Write-Output "Completed"
